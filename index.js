477d994b07f1b84e61b1f7a880eb82812b0644b0
MdTarikulIslam <nayon1820@gmail.com>
2023-05-01 12:34:04 +0600

    mongo added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -16,8 +17,44 @@ const users = [
   { id: 3, name: "islam", email: "islam@gmail.com" },
 ];
 
+
+
+const uri = "mongodb+srv://tarikul:4KR1oeYdI61XEPEy@cluster0.iolwevh.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get("/users", (req, res) => {
-  res.send(users);
+  if (req.query.name) {
+    const search = req.query.name;
+    const filtered = users.filter((usr) =>
+      usr.name.toLowerCase().indexOf(search)
+    );
+    res.send(filtered);
+  } else {
+    res.send(users);
+  }
 });
 
 app.post("/users", (req, res) => {
@@ -32,3 +69,9 @@ app.post("/users", (req, res) => {
 app.listen(port, () => {
   console.log(`Simple node server is running on port ${port}`);
 });
+
+
+/**
+ * user:tarikul
+ * password: 4KR1oeYdI61XEPEy
+ */
